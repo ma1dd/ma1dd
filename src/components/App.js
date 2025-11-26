3d5670e673b1d79d3a91c1e692204e813dfdde8e
NIXXXON177 <kudrasovn024@gmail.com>
2025-11-26 22:54:09 +0300

    4

--- src/components/App.js
+++ src/components/App.js
@@ -1,138 +1,146 @@
 // src/components/App.js
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from '../contexts/AuthContext';
-import Layout from './layout/Layout';
-import Login from './Login';
-import Home from './Home';
-import Products from './Products';
-import ProductDetail from './ProductDetail';
-import Sessions from './Sessions';
-import SessionDetail from './SessionDetail';
-import ProfileSelfRedirect from './ProfileSelfRedirect';
-import Profile from './Profile';
-import EditProfile from './EditProfile';
-import AdminPanel from './AdminPanel';
-import CreateSession from './CreateSession';
-import ProtectedRoute from './ProtectedRoute';
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom'
+import { AuthProvider } from '../contexts/AuthContext'
+import Layout from './layout/Layout'
+import Login from './Login'
+import Home from './Home'
+import Products from './Products'
+import ProductDetail from './ProductDetail'
+import Sessions from './Sessions'
+import SessionDetail from './SessionDetail'
+import ProfileSelfRedirect from './ProfileSelfRedirect'
+import Profile from './Profile'
+import EditProfile from './EditProfile'
+import AdminPanel from './AdminPanel'
+import CreateSession from './CreateSession'
+import ProtectedRoute from './ProtectedRoute'
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Публичный маршрут - страница входа */}
-          <Route path="/login" element={<Login />} />
-          
-          {/* Защищенные маршруты */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Home />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/products"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Products />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/products/:productId"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <ProductDetail />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/sessions"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Sessions />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/sessions/:sessionId"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <SessionDetail />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/create_session"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <CreateSession />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <ProfileSelfRedirect />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile/:userId"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Profile />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit_profile"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <EditProfile />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* Маршрут администратора */}
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute requireAdmin={true}>
-                <Layout>
-                  <AdminPanel />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* Перенаправление несуществующих маршрутов */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
-};
+	// Для GitHub Pages используем basename
+	const basename = process.env.NODE_ENV === 'production' ? '/ma1dd' : '/'
 
-export default App;
+	return (
+		<AuthProvider>
+			<Router basename={basename}>
+				<Routes>
+					{/* Публичный маршрут - страница входа */}
+					<Route path='/login' element={<Login />} />
+
+					{/* Защищенные маршруты */}
+					<Route
+						path='/'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<Home />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/products'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<Products />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/products/:productId'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<ProductDetail />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/sessions'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<Sessions />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/sessions/:sessionId'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<SessionDetail />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/create_session'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<CreateSession />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/profile'
+						element={
+							<ProtectedRoute>
+								<ProfileSelfRedirect />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/profile/:userId'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<Profile />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/edit_profile'
+						element={
+							<ProtectedRoute>
+								<Layout>
+									<EditProfile />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+
+					{/* Маршрут администратора */}
+					<Route
+						path='/admin'
+						element={
+							<ProtectedRoute requireAdmin={true}>
+								<Layout>
+									<AdminPanel />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+
+					{/* Перенаправление несуществующих маршрутов */}
+					<Route path='*' element={<Navigate to='/' replace />} />
+				</Routes>
+			</Router>
+		</AuthProvider>
+	)
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,67 @@
 // webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js', // Точка входа
-  output: {
-    path: path.resolve(__dirname, 'build'), // Папка для выходных файлов
-    filename: 'bundle.js', // Имя выходного файла
-    publicPath: '/', // Важно для SPA
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // Путь к шаблону index.html
-      filename: 'index.html', // Имя выходного файла в dist
-      inject: 'body', // Вставлять скрипт в body
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx'], // Позволяет импортировать без указания расширения
-  },
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-      {
-        directory: path.join(__dirname, 'public'),
-        publicPath: '/',
-      },
-    ],
-    compress: true,
-    port: 8080,
-    open: true,
-    historyApiFallback: true, // Очень важно для React Router и SPA!
-  },
-  mode: 'development',
-};
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production'
+
+	return {
+		entry: './src/index.js', // Точка входа
+		output: {
+			path: path.resolve(__dirname, 'build'), // Папка для выходных файлов
+			filename: 'bundle.js', // Имя выходного файла
+			publicPath: isProduction ? '/ma1dd/' : '/', // Для GitHub Pages используем /ma1dd/
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react'],
+						},
+					},
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './public/index.html', // Путь к шаблону index.html
+				filename: 'index.html', // Имя выходного файла в dist
+				inject: 'body', // Вставлять скрипт в body
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{ from: 'public/data', to: 'data' },
+					{ from: 'public/.nojekyll', to: '.nojekyll', toType: 'file' },
+				],
+			}),
+		],
+		resolve: {
+			extensions: ['.js', '.jsx'], // Позволяет импортировать без указания расширения
+		},
+		devServer: {
+			static: [
+				{
+					directory: path.join(__dirname, 'dist'),
+				},
+				{
+					directory: path.join(__dirname, 'public'),
+					publicPath: '/',
+				},
+			],
+			compress: true,
+			port: 8080,
+			open: true,
+			historyApiFallback: true, // Очень важно для React Router и SPA!
+		},
+		mode: argv.mode || 'development',
+	}
+}
